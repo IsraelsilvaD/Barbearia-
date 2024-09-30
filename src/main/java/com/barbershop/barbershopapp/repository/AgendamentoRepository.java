351d2c278872868984565b7703f247aefc54ceeb
IsraelsilvaD <130614485+IsraelsilvaD@users.noreply.github.com>
2024-09-30 19:50:02 -0300

    Update AgendamentoRepository.java Alteração

--- src/main/java/com/barbershop/barbershopapp/repository/AgendamentoRepository.java
+++ src/main/java/com/barbershop/barbershopapp/repository/AgendamentoRepository.java
@@ -2,10 +2,17 @@ package com.trimtime.app.repository;
 
 import com.trimtime.app.domain.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+    
+    // Método para encontrar agendamentos de um barbeiro em um horário específico
     List<Agendamento> findByBarbeiroIdAndDataAgendamento(Long barbeiroId, LocalDateTime dataAgendamento);
+
+    // Método para encontrar agendamentos de um cliente
+    List<Agendamento> findByClienteId(Long clienteId);
 }
