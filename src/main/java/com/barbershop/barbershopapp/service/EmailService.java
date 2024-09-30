a3fd895f0ce7f94b8dda4e0df15a02f0a49a804a
IsraelsilvaD <130614485+IsraelsilvaD@users.noreply.github.com>
2024-09-30 16:53:00 -0300

    Update EmailService.java Atualização

--- src/main/java/com/barbershop/barbershopapp/service/EmailService.java
+++ src/main/java/com/barbershop/barbershopapp/service/EmailService.java
@@ -1,4 +1,4 @@
-package com.barbershop.barbershopapp.service;
+package com.trimtime.app.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
