93ff46ed3f0a940591464e06dcac392a482d6069
IsraelsilvaD <130614485+IsraelsilvaD@users.noreply.github.com>
2024-09-30 16:30:22 -0300

    Update OrderRepository.java Atualização

--- src/main/java/com/barbershop/barbershopapp/repository/OrderRepository.java
+++ src/main/java/com/barbershop/barbershopapp/repository/OrderRepository.java
@@ -1,7 +1,7 @@
-package com.barbershop.barbershopapp.repository;
+package com.trimtime.app.repository;
 
-import com.barbershop.barbershopapp.domain.Order;
-import com.barbershop.barbershopapp.domain.User;
+import com.trimtime.app.domain.Order;
+import com.trimtime.app.domain.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
