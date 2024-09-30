dab2aec13d25b8202e2c689efc35c33ccdba363a
IsraelsilvaD <130614485+IsraelsilvaD@users.noreply.github.com>
2024-09-30 19:48:42 -0300

    Update AgendamentoService.java Alteração

--- src/main/java/com/barbershop/barbershopapp/service/AgendamentoService.java
+++ src/main/java/com/barbershop/barbershopapp/service/AgendamentoService.java
@@ -14,19 +14,24 @@ public class AgendamentoService {
     @Autowired
     private AgendamentoRepository agendamentoRepository;
 
-    // Método para verificar se o horário está disponível
+    // Método para verificar se o horário está disponível para um barbeiro específico
     public boolean isHorarioDisponivel(Long barbeiroId, LocalDateTime dataAgendamento) {
         List<Agendamento> agendamentos = agendamentoRepository.findByBarbeiroIdAndDataAgendamento(barbeiroId, dataAgendamento);
         return agendamentos.isEmpty(); // Retorna true se não houver agendamento para o horário
     }
 
     // Método para salvar o agendamento
-    public Agendamento salvarAgendamento(Long clienteId, Long barbeiroId, LocalDateTime dataAgendamento, Long servicoId) {
+    public Agendamento salvarAgendamento(Long clienteId, Long barbeiroId, Long servicoId, LocalDateTime dataAgendamento, String profissional) {
         if (isHorarioDisponivel(barbeiroId, dataAgendamento)) {
-            Agendamento agendamento = new Agendamento(clienteId, barbeiroId, dataAgendamento, servicoId);
+            Agendamento agendamento = new Agendamento(clienteId, barbeiroId, profissional, servicoId, dataAgendamento.toLocalDate(), dataAgendamento.toLocalTime());
             return agendamentoRepository.save(agendamento);
         } else {
             throw new RuntimeException("Horário não disponível");
         }
+    }
+
+    // Método para listar todos os agendamentos de um cliente
+    public List<Agendamento> listarAgendamentosPorCliente(Long clienteId) {
+        return agendamentoRepository.findByClienteId(clienteId);
     }
 }
